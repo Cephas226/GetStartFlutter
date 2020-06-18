e4065c2e3043d98da0d60179ee3f75ee4d99b6ec
Cooly <35110561+lanezozor@users.noreply.github.com>
2020-06-18 21:24:03 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -84,6 +84,7 @@ class MyApp extends StatelessWidget {
                           },
                         ),
                         _buildDivider(),
+      //----Change here
                        _buildListTile(context),
                         _buildDivider(),
                         ListTile(
